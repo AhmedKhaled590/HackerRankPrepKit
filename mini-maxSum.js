4a63f2ee4dea46caeb37c03b1e589eabaec77c2f
AhmedKhaled590 <kahmd1444@gmail.com>
2022-07-12 01:03:53 +0200

    week1

---
+++ mini-maxSum.js
@@ -0,0 +1,43 @@
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function miniMaxSum(arr) {
+  arr = arr.sort();
+  let minSum = arr.slice(0, 4).reduce((a, b) => a + b, 0);
+  let maxSum = arr.slice(1, 5).reduce((a, b) => a + b, 0);
+  console.log(minSum + " " + maxSum);
+}
+
+function main() {
+  const arr = readLine()
+    .replace(/\s+$/g, "")
+    .split(" ")
+    .map((arrTemp) => parseInt(arrTemp, 10));
+
+  miniMaxSum(arr);
+}
