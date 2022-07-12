f57b31f1931d60d8627d153a987d16622a8bb5d3
AhmedKhaled590 <kahmed1444@gmail.com>
2022-07-12 17:55:49 +0200

    Week1 Done

--- week1/breakingTheRecords.js
+++ week1/breakingTheRecords.js
@@ -68,6 +68,35 @@ function breakingRecords(scores) {
   return result;
 }
 
+/**
+ * Optimized Solution O(N)
+ 
+function divisibleSumPairs(k, ar) {
+  // Write your code here
+  const map = new Map();
+  let noOfPair = 0;
+
+  for (let i = 0; i < ar.length; i++) {
+    const remainder = ar[i] % k;
+    let diff = k - remainder;
+    if (diff === k) {
+      // means ar[i] is a divisible
+      diff = 0;
+    }
+
+    if (map.has(diff)) {
+      noOfPair += map.get(diff);
+    }
+    if (map.has(remainder)) {
+      let count = map.get(remainder);
+      map.set(remainder, count + 1);
+    } else {
+      map.set(remainder, 1);
+    }
+  }
+
+  return noOfPair;
+}*/
 function main() {
   const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
 
---
+++ week1/camelCase4.js
@@ -0,0 +1,56 @@
+function processData(data) {
+  let inputs = data.split("\r\n");
+  inputs.forEach((input) => {
+        let operation = input[0];
+    let type = input[2];
+    let lastIndex = type === "M" ? input.length - 2 : input.length;
+    if (operation === "S") {
+      let capitalPositions = [0];
+      let ans = [];
+
+      input = input.slice(4, lastIndex);
+
+      for (let i = 0; i < input.length; i++) {
+        element = input[i];
+        if (element >= "A" && element < "Z") {
+          capitalPositions.push(i);
+        }
+      }
+      capitalPositions.push(input.length);
+      for (let j = 0; j < capitalPositions.length - 1; j++) {
+        ans.push(
+          input
+            .slice(capitalPositions[j], capitalPositions[j + 1])
+            .toLowerCase()
+        );
+      }
+      console.log(ans.join(" ").trimStart());
+      return;
+    } else if (operation === "C") {
+      let ans = "";
+
+      input = input.slice(4, input.length);
+      let words = input.split(" ");
+      ans =
+        type === "C"
+          ? words[0].charAt(0).toUpperCase() + words[0].slice(1)
+          : words[0];
+      for (let j = 1; j < words.length; j++) {
+        ans += words[j].charAt(0).toUpperCase() + words[j].slice(1);
+      }
+      ans = type === "M" ? (ans += "()") : ans;
+      console.log(ans);
+    }
+  });
+}
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+  _input += input;
+});
+
+process.stdin.on("end", function () {
+  processData(_input);
+});
