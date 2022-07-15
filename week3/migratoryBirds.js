761db94e4ed124301e06874a93d305c53fec7fbc
AhmedKhaled590 <kahmed1444@gmail.com>
2022-07-15 14:14:57 +0200

    w3:6

---
+++ week3/migratoryBirds.js
@@ -0,0 +1,62 @@
+"use strict";
+
+const fs = require("fs");
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'migratoryBirds' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function migratoryBirds(arr) {
+  let map = new Array(5).fill(0);
+  for (let i = 0; i < arr.length; i++) {
+    map[arr[i] - 1]++;
+  }
+  let max = -Infinity;
+  let maxIndex = 0;
+  for (let i = 0; i < 5; i++) {
+    let temp = map[i] > max ? map[i] : max;
+    maxIndex = map[i] == max ? maxIndex : map[i] > max ? i : maxIndex;
+    max = temp;
+  }
+  return maxIndex + 1;
+}
+
+function main() {
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+  const arrCount = parseInt(readLine().trim(), 10);
+
+  const arr = readLine()
+    .replace(/\s+$/g, "")
+    .split(" ")
+    .map((arrTemp) => parseInt(arrTemp, 10));
+
+  const result = migratoryBirds(arr);
+
+  ws.write(result + "\n");
+
+  ws.end();
+}
