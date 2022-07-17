009fab70dc8b188db736b7dbf3f5add1272bdd6b
AhmedKhaled590 <kahmed1444@gmail.com>
2022-07-17 18:43:58 +0200

    w4:7-test

--- week4/leftRotation.js
+++ week4/leftRotation.js
@@ -1,68 +1,68 @@
-    "use strict";
+"use strict";
 
-    const fs = require("fs");
+const fs = require("fs");
 
-    process.stdin.resume();
-    process.stdin.setEncoding("utf-8");
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
 
-    let inputString = "";
-    let currentLine = 0;
+let inputString = "";
+let currentLine = 0;
 
-    process.stdin.on("data", function (inputStdin) {
-    inputString += inputStdin;
-    });
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
+});
 
-    process.stdin.on("end", function () {
-    inputString = inputString.split("\n");
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
 
-    main();
-    });
+  main();
+});
 
-    function readLine() {
-    return inputString[currentLine++];
-    }
+function readLine() {
+  return inputString[currentLine++];
+}
 
-    /*
-    * Complete the 'rotateLeft' function below.
-    *
-    * The function is expected to return an INTEGER_ARRAY.
-    * The function accepts following parameters:
-    *  1. INTEGER d
-    *  2. INTEGER_ARRAY arr
-    */
+/*
+ * Complete the 'rotateLeft' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER d
+ *  2. INTEGER_ARRAY arr
+ */
 
-    /**
-     * mod of neg number returned as positive
-     */
-    function mod(x, y) {
-    return ((x % y) + y) % y;
-    }
+/**
+ * mod of neg number returned as positive
+ */
+function mod(x, y) {
+  return ((x % y) + y) % y;
+}
 
-    function rotateLeft(d, arr) {
-    let newArr = new Array(arr.length).fill(0);
-    for (let i = 0; i < arr.length; i++) {
-        newArr[mod(i - d, arr.length)] = arr[i];
-    }
-    return newArr;
-    }
+function rotateLeft(d, arr) {
+  let newArr = new Array(arr.length).fill(0);
+  for (let i = 0; i < arr.length; i++) {
+    newArr[mod(i - d, arr.length)] = arr[i];
+  }
+  return newArr;
+}
 
-    function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+function main() {
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
 
-    const firstMultipleInput = readLine().replace(/\s+$/g, "").split(" ");
+  const firstMultipleInput = readLine().replace(/\s+$/g, "").split(" ");
 
-    const n = parseInt(firstMultipleInput[0], 10);
+  const n = parseInt(firstMultipleInput[0], 10);
 
-    const d = parseInt(firstMultipleInput[1], 10);
+  const d = parseInt(firstMultipleInput[1], 10);
 
-    const arr = readLine()
-        .replace(/\s+$/g, "")
-        .split(" ")
-        .map((arrTemp) => parseInt(arrTemp, 10));
+  const arr = readLine()
+    .replace(/\s+$/g, "")
+    .split(" ")
+    .map((arrTemp) => parseInt(arrTemp, 10));
 
-    const result = rotateLeft(d, arr);
+  const result = rotateLeft(d, arr);
 
-    ws.write(result.join(" ") + "\n");
+  ws.write(result.join(" ") + "\n");
 
-    ws.end();
-    }
+  ws.end();
+}
